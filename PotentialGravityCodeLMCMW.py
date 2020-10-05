0d6b3b2dd891979f3855535f7b8059ba48ed0071
ErwanH29 <71374941+ErwanH29@users.noreply.github.com>
2020-10-05 21:32:56 +0200

    Add files via upload

--- PotentialGravityCodeLMCMW.py
+++ PotentialGravityCodeLMCMW.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 from amuse.units import units, constants
 from amuse.plot import plot
 from astropy import units as u
+import astropy.coordinates as coord
 from astropy.coordinates import SkyCoord
 from amuse.ext.galactic_potentials import MWpotentialBovy2015, MiyamotoNagai_profile, NFW_profile, Plummer_profile
 
-#Converting coordinate systems to Cartesian
-c = SkyCoord(ra=79.88*u.degree, dec=-69.59*u.degree, distance=49.97*u.kpc) #Data of LMC distance taken from Pieterzxynski et al 2013 in the Sun's reference frame
-xcordLMC = c.cartesian.x ; ycordLMC = c.cartesian.y ; zcordLMC = c.cartesian.z
-#The Sun is at (8.3, 0.027, 0) kpc from the center of the Milky Way and moving at (v_R, v_theta, v_z) = (-11.1, 232.24, 7.25) km/s (From Chen et al. 2001, Gillessen et al. 2009, Bovy 2015)
-#We can use astropy to convert into a galactocentric frame as well as convert the proper motion values of the HVS3
+#Initialising LMC
+cLMC = SkyCoord(ra=79.88*u.degree, dec=-69.59*u.degree, distance=49.97*u.kpc) #Data of LMC distance taken from Pieterzxynski et al 2013 in the Sun's reference frame
+gcLMC = cLMC.transform_to(coord.Galactocentric)
+print("LMC coordinates in galactocentric frame: \n x = ", gcLMC.cartesian.x, ", y = ", gcLMC.cartesian.y, ", z = ", gcLMC.cartesian.z)
 
 #HVS initial conditions - Taken from Gaia Dr2
-HVS3RADEC = SkyCoord("04:38:12.8 -54:33:12", unit=(u.hourangle, u.deg)) #Converting R.A and Dec of HVS3 to degrees - may have to check if this is correct and if we can convert in same line to MW frame
-c = SkyCoord(ra=HVS3RADEC.ra.degree*u.degree, dec=HVS3RADEC.dec.degree*u.degree, distance=61*u.kpc) #Converting degrees into cartesian coordinates in the Sun's reference frame
-xcordHVS3 = c.cartesian.x ; ycordHVS3 = c.cartesian.z ; zcordHVS3 = c.cartesian.z
-
-
-print("Coordinates for LMC: ", xcordLMC, ",", ycordLMC, ",", zcordLMC)
-print("Coordinates for HVS3: ", xcordHVS3, ",", ycordHVS3, ",", zcordHVS3)
+HVS3RADEC = SkyCoord("04:38:12.8 -54:33:12", unit=(u.hourangle, u.deg)) #Converting R.A and Dec of HVS3 to degrees
+cS = coord.ICRS(ra=HVS3RADEC.ra.degree*u.degree, dec=HVS3RADEC.dec.degree*u.degree, 
+                distance=61*u.kpc, pm_ra_cosdec=0.851*u.mas/u.yr, 
+                pm_dec=1.936*u.mas/u.yr, radial_velocity=723*u.km/u.s)
+gcS = cS.transform_to(coord.Galactocentric) #Setting HVS3 coordinates with respect to the center of the MW
+print("HVS3 coordinates in galactocentric frame: \n x = ", gcS.cartesian.x, ", y = ", gcS.cartesian.y, ", z = ", gcS.cartesian.z)
+print("HVS3 velocity in galactocentric frame: \n vx = ", gcS.v_x, ", vy = ", gcS.v_y, ", vz = ", gcS.v_z)
 
 #Defining an x,y,z space to plot in
 D = 100
@@ -49,21 +49,61 @@ LMCNFW = LMCNFWProfile.get_potential_at_point(eps, x, y, z)
 CombinedLMC = (LMCNFW + LMCPlummer)
 pot_MW = MW.get_potential_at_point(eps, xMW, yMW, zMW)
 
+r = (x**2+y**2+z**2)**0.5
+
+#Getting the circular velocity as a function of distance for the different potentials
+vcircMW = MW.circular_velocity(r).in_(units.km/units.s)
+vcircLMC = LMCPlummerProfile .circular_velocity(r).in_(units.km/units.s)+LMCNFWProfile.circular_velocity(r).in_(units.km/units.s)
+
+
+
+#PLOTTING THE ORBITAL VELOCITY AROUND GALAXIES AND THE POTENTIALS
+plt.title(r"Milky Way Circular Velocity ($v_{circ}$) vs Distance ($r$)")
+plt.xlabel(r"Distance ($r$)")
+plt.ylabel(r"Velocity $v_{circ}$")
+plot(x, vcircMW, label = 'Milky Way Potential')
+plot(x, vcircLMC, label = 'LMC Potential Potential')
+plt.xlim(0,20)
+plt.legend()
+plt.savefig("MW+LMC Circular Velocity", dpi=300)
+plt.show()  
+
+plt.title(r"Milky Way Circular Velocity ($v_{circ}$) vs Distance ($r$)")
+plt.xlabel(r"Distance ($r$)")
+plt.ylabel(r"Velocity $v_{circ}$")
+plot(x, vcircMW, label = 'Milky Way Potential')
+plt.xlim(0,50)
+plt.legend()
+plt.savefig("MW Circular Velocity", dpi=300)
+plt.show() 
+
+plt.title(r"LMC Circular Velocity ($v_{circ}$) vs Distance ($r$)")
+plt.xlabel(r"Distance ($r$)")
+plt.ylabel(r"Velocity $v_{circ}$")
+plot(x, vcircMW, label = 'LMC Potential')
+plt.xlim(0,50)
+plt.legend()
+plt.savefig("LMC Circular Velocity", dpi=300)
+plt.show()  
+
 plt.title("Milky Way and LMC Potentials")
 plot(x, pot_MW, label = 'Milky Way Potential')
 plot(x, CombinedLMC, label = 'Combined')
 plt.xlim(0,D)
 plt.legend()
+plt.savefig("MW and LMC Potentials", dpi=300)
 plt.show()   
 
 plt.title("LMC NFW Potential")
 plot(x, LMCNFW, label = 'NFW')
 plt.xlim(0,D)
 plt.legend()
+plt.savefig("LMC NFW Potential", dpi=300)
 plt.show()   
 
 plt.title("LMC Plummer Potential")
 plot(x, LMCPlummer, label = 'Plummer Profile (LMC)')
 plt.xlim(0,30)
 plt.legend()
+plt.savefig("LMC Plummer Potential", dpi=300)
 plt.show()   
