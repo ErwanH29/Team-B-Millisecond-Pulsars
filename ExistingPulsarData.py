aa762741ca1f4deced26298c1f97eed4c87d4c36
ErwanH29 <71374941+ErwanH29@users.noreply.github.com>
2020-10-08 19:45:45 +0200

    Add files via upload

--- ExistingPulsarData.py
+++ ExistingPulsarData.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("Desktop\PulsarsDataATNF(2019-04-24).txt", comments="#") #Importing milisecond pulsar data
-print(data[2,3])
+data = np.loadtxt("Desktop\PulsarsDataATNF(2019-04-24).txt", comments="#", usecols=(6,7, 8)) #Importing milisecond pulsar data
 for i in range(0,len(data)):
-    plt.scatter(data[i,3], data[i,4])
+    plt.scatter(data[i,1], data[i,2])
 plt.show()
