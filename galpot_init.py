737771c04a9452c2053523fac14f33868790867e
Shiyang Zhang <56381572+Shiyang1043@users.noreply.github.com>
2020-10-10 14:14:15 +0200

    Update galpot_init.py

--- galpot_init.py
+++ galpot_init.py
@@ -1,16 +1,22 @@
-#This code is to define an LMC potential and place it at the right galactrocentric coordinates
+#This code is to define an LMC & SMC potential and place it at the right galactrocentric coordinates
 from amuse.lab import units
 from conv_coord import conv_coord, prop_motion
 from amuse.ext.galactic_potentials import Plummer_profile, NFW_profile
 
 class LMC_pot(object):
-    def __init__(self):
+    def __init__(self, ra, dec, dis, gc = False):
           
         self.plum = Plummer_profile(2e10|units.MSun, 0.73|units.kpc)
         self.nfw = NFW_profile(8.18e6|units.MSun/units.kpc**3, #This value was taken from SIffert et al. 2011
                                2.6|units.kpc)
-        
-        self.d = conv_coord(79.88, -69.59, 49.97)
+        self.ra = ra
+        self.dec = dec
+        self.dis = dis
+        if gc == Ture:
+            self.d = (self.ra, self.dec, self.dis) | units.kpc
+        else:
+            self.d = conv_coord(self.ra, self.dec, self.dis)
+        #self.d = conv_coord(79.88, -69.59, 49.97)
         #self.v = prop_motion(1.91, 0.229, 262)
         
     def get_potential_at_point(self, eps, x, y, z):
@@ -35,12 +41,18 @@ class LMC_pot(object):
         return ax_p+ax_h, ay_p+ay_h, az_p+az_h
 
 class SMC_pot(object):
-    def __init__(self):
+    def __init__(self, ra, dec, dis, gc = False):
           
         self.plum = Plummer_profile(4e9|units.MSun, 0.339|units.kpc) # scale radius by assuming R~m^(1/3) and comparing to LMC
         self.nfw = NFW_profile(8.18e6|units.MSun/units.kpc**3, # No values for SMC halo....
                                2.6|units.kpc)
-        
+        self.ra = ra
+        self.dec = dec
+        self.dis = dis
+        if gc == True:
+            self.d = (self.ra, self.dec, self.dis) | units.kpc
+        else:
+            self.d = conv_coord(self.ra, self.dec, self.dis)
         self.d = conv_coord(13.16, -72.8, 60.6) # taken from SIMBAD
         #self.v = prop_motion(0.772, -1.117, 145.6)
      
