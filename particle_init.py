f0a96d387739a0c974d1a64a8ba88063ba47330d
ErwanH29 <71374941+ErwanH29@users.noreply.github.com>
2020-10-12 22:59:58 +0200

    Add files via upload

--- particle_init.py
+++ particle_init.py
@@ -1,20 +1,50 @@
 from amuse.lab import Particles, units
+import numpy as np
+import random
 
 # initialize particle sets here. 
 ##### NEUTRON STAR SET #####
 
-def neut_stars(N_neut):
-    return neuts
+class neut_initialiser(object):
+    def ProbFunc(vel):
+        meanV = 300 ; sigmaV = 190
+        return np.sqrt(2/np.pi)*(meanV**2/sigmaV**3)*np.exp(-vel**2/(2*sigmaV**2))
 
-def neut_path_init(N_neut):
-    for i in range(N_neut):
-        x_neut = [] | units.kpc
-        y_neut = [] | units.kpc
-        z_neut = [] | units.kpc
-        #X_neut[i] = (x_neut, y_neut, z_neut)
-    return 
+    def velocityList(vrange, w, N):
+        return random.choices(vrange, weights=w, 
+                              k = N) | units.km/units.s   
 
-neuts = neut_path_init(10)
+    def massList(N):
+        meanM = 1.4 ; sigmaMass = 0.3
+        return np.random.normal(meanM, sigmaMass, N) | units.MSun 
+
+    N = int(input("Give the number of millisecond pulsars you want to simulate: ", ))    
+    dataVels = np.loadtxt("VELOCITIESPulsarsDataATNF(2019-04-24).txt", 
+                          comments="#", usecols=(3, 6, -1))
+    
+    vrange = np.linspace(200, max(dataVels[:,-1]), N)
+    velocityDistr = ProbFunc(vrange)
+    velList = velocityList(vrange, velocityDistr, N)
+    
+    x = np.zeros(N) |units.kpc 
+    y = np.zeros(N) | units.kpc 
+    z = np.zeros(N) | units.kpc
+    
+    neut_InitialConditions = np.stack((x, y, z, velList, 
+                                       massList(N)), axis=1)
+    
+    def neut_stars(N_neut):
+        return neuts
+
+    def neut_path_init(N_neut):
+        for i in range(N_neut):
+            x_neut = [] | units.kpc
+            y_neut = [] | units.kpc
+            z_neut = [] | units.kpc
+            #X_neut[i] = (x_neut, y_neut, z_neut)
+        return 
+
+#neuts = neut_path_init(10)
 ##### GALAXY TRACERS #####
 
 # initialize LMC and SMC
