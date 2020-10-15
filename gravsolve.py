454ca1a59d69fdbb7e60d50d2b8d84da414aec44
Shiyang Zhang <56381572+Shiyang1043@users.noreply.github.com>
2020-10-15 17:31:37 +0200

    Update gravsolve.py

--- gravsolve.py
+++ gravsolve.py
@@ -5,8 +5,9 @@ from evol import neut_gal_evol
 
 X_gal, X_neut = neut_gal_evol()
 
-plot(X_gal[0], X_gal[1], lw=1)
-plot(X_gal[3], X_gal[4], lw=1)
-plot(X_neut[0], X_neut[1], lw=1)
+plot(X_gal[0], X_gal[1], lw=1, lable = 'lmc')
+plot(X_gal[3], X_gal[4], lw=1, lable = 'smc')
+plot(X_neut[0], X_neut[1], lw=1, lable = 'neut')
 #pyplot.gca().set_aspect("equal", adjustable="box")
+pyplot.legend()
 pyplot.show()
