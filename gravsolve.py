a565ecebe323609accf0c53deee4c36edb5deefd
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-21 15:27:19 +0200

    Update gravsolve.py

--- gravsolve.py
+++ gravsolve.py
@@ -2,13 +2,17 @@
 from matplotlib import pyplot
 from amuse.plot import *
 from evol import neut_gal_evol
+from capture_check import capture_check
 
-X_gal, X_neut = neut_gal_evol()
+X_gal, X_neut, N = neut_gal_evol()
 
 plot(X_gal[0], X_gal[1], lw=0.7, label='LMC')
 plot(X_gal[3], X_gal[4], lw=0.7, label='SMC')
-plot(X_neut[0][0], X_neut[0][1], lw=1, label='Pulsar 1')
-plot(X_neut[1][0], X_neut[1][1], lw=1, label='Pulsar 2')
+
+check = capture_check(X_neut)
+
+print(check)
+
 #pyplot.gca().set_aspect("equal", adjustable="box")
 # plot the isocontour 
 from amuse.ext.galactic_potentials import MWpotentialBovy2015
@@ -21,12 +25,17 @@ effective_iso_potential_plot(MW, omega, center_of_rotation = [0, 0]|units.kpc,
                                   linewidths=0.5, cmap=('Greys')), omega2 = None, center_of_rotation2 = [0, 0]|units.kpc, 
                                   fraction_screen_filled2 = 0.2, projection3D = False, number_of_contours = 15)
 
-pyplot.colorbar()
+#pyplot.colorbar()
 
 pyplot.xlabel(r"$x$ coordinate (kpc)")
 pyplot.ylabel(r"$y$ coordinate (kpc)")
 pyplot.xlim(-20,20)
 pyplot.ylim(-40,30)
+
+
+for i in range(N):
+    plot(X_neut[i][0], X_neut[i][1], lw=1, label='Pulsar {}'.format(i+1))
+
 pyplot.legend()
 pyplot.savefig("EvolutionSystem", dpi=300)
 pyplot.show()
