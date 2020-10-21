1ca6024e7c79329cb1686bacac90a9c57f610c94
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-21 11:18:08 +0200

    Update particle_init.py
    added some changes to the neutron star code

--- particle_init.py
+++ particle_init.py
@@ -1,82 +1,84 @@
 from amuse.lab import Particles, units
 import numpy as np
 import random
+from conv_coord import conv_coord
 
 # initialize particle sets here. 
 ##### NEUTRON STAR SET #####
 
-class neut_initialiser(object):
-    def ProbFunc(vel):
+class neut_initializer(object):
+    
+    def __init__(self):
+        self.N = int(input("Give the number of millisecond pulsars you want to simulate: ", ))
+        self.dataVels = np.loadtxt("VELOCITIESPulsarsDataATNF(2019-04-24).txt", 
+                                   comments="#", usecols=(3, 6, -1))
+    
+    def N(self):
+        N = self.N
+        return N
+        
+    def ProbFunc(self, vel):
         meanV = 300 ; sigmaV = 190
         return np.sqrt(2/np.pi)*(meanV**2/sigmaV**3)*np.exp(-vel**2/(2*sigmaV**2))
 
-    def velocityList(vrange, w, N):
-        scalex = np.random.choice([-1,1], N) ; scaley = np.random.choice([-1,1], N)
-        scalez = np.random.choice([-1,1], N)
-        vx = random.choices(vrange, weights=w, k = N)*scalex * (1 | units.kms) + \
-        (47) * (1 | units.kms) 
-        vy = random.choices(vrange, weights=w, k = N)*scaley * (1 | units.kms) + \
-        (242) * (1 | units.kms) 
-        vz = random.choices(vrange, weights=w, k = N)*scalez * (1 | units.kms) + \
-        (225) * (1 | units.kms) 
-        return    vx, vy, vz
+    def velocityList(self, vrange, w):
+        r=np.linspace(-1,1,1000)
+        scalex = [np.random.choice(r)]
+        scaley = [np.random.choice(r)]
+        scalez = [np.random.choice(r)]
+        vx = np.array(random.choices(vrange, weights=w, k = 1))*scalex
+        vy = np.array(random.choices(vrange, weights=w, k = 1))*scaley
+        vz = np.array(random.choices(vrange, weights=w, k = 1))*scalez
+        return vx, vy, vz
 
-    def massList(N):
+    def massList(self):
         meanM = 1.4 ; sigmaMass = 0.3
-        return np.random.normal(meanM, sigmaMass, N) #| units.MSun 
+        return np.random.normal(meanM, sigmaMass) #| units.MSun 
 
-    N = int(input("Give the number of millisecond pulsars you want to simulate: ", ))    
-    dataVels = np.loadtxt("VELOCITIESPulsarsDataATNF(2019-04-24).txt", 
-                          comments="#", usecols=(3, 6, -1))
-    
-    vrange = np.linspace(200, max(dataVels[:,-1]), N)/3
-    velocityDistr = ProbFunc(vrange)
-    velListx, velListy, velListz  = velocityList(vrange, velocityDistr, N)
-    velocityList = np.transpose(np.stack((velListx, velListy, velListz, massList(N))))
+    def neut_path_init(self):
 
-    x = np.zeros(N) #| units.kpc 
-    y = np.zeros(N) #| units.kpc 
-    z = np.zeros(N) #| units.kpc
-    
-    neut_InitialConditions = np.stack((x, y, z), axis=1)
-    neutCords = [0 for i in range(N)]
-    for i in range(N):
-        neutCords[i] = neut_InitialConditions[i, :3]
-    
-    #def neut_stars(N_neut):
-     #   return neuts
+        x_neut = [] | units.kpc 
+        y_neut = [] | units.kpc 
+        z_neut = [] | units.kpc
+        return x_neut, y_neut, z_neut
 
-    #def neut_path_init(N_neut):
-     #   for i in range(N_neut):
-      #      x_neut = [] | units.kpc
-       #     y_neut = [] | units.kpc
-        #    z_neut = [] | units.kpc
-         #   #X_neut[i] = (x_neut, y_neut, z_neut)
-        #return 
+    def neut_stars(self):
+        vrange = np.linspace(200, max(self.dataVels[:,-1]))/3
+        velocityDistr = self.ProbFunc(vrange)
+        neuts = Particles(self.N)
+        vx, vy, vz = self.velocityList(vrange, velocityDistr)
+        print(vx, vy, vz)
+        for n in range(self.N):
+            neut = neuts[n]
+            neut.mass = self.massList() | units.MSun
+            neut.position = conv_coord(74.7875, -65.9878, 49)
+            neut.velocity = (vx, vy, vz) | units.kms
+            neut.velocity += (47,242,225) * (1 | units.kms)
+        return neuts
 
-#neuts = neut_path_init(10)
 ##### GALAXY TRACERS #####
 
 # initialize LMC and SMC
-def gal_tracer():
-    gals = Particles(2)
-    gal_lmc = gals[0]
-    gal_lmc.mass = 2e10 | units.MSun 
-    gal_lmc.position = (-0.88,-41,-27) * (1 | units.kpc)
-    gal_lmc.velocity = (47,242,225) * (1 | units.kms)
-
-    gal_smc = gals[1]
-    gal_smc.mass = 2e10 | units.MSun 
-    gal_smc.position = (15,-36,-42) * (1 | units.kpc)
-    gal_smc.velocity = (5.35,164,136) * (1 | units.kms)
-    return gals
-
-def gal_path_init():
-     x_lmc = [] | units.kpc
-     y_lmc = [] | units.kpc
-     z_lmc = [] | units.kpc
-
-     x_smc = [] | units.kpc
-     y_smc = [] | units.kpc
-     z_smc = [] | units.kpc
-     return x_lmc, y_lmc, z_lmc, x_smc, y_smc, z_smc 
+class gal_initializer(object):
+    def gal_tracer(self):
+        gals = Particles(2)
+        gal_lmc = gals[0]
+        gal_lmc.mass = 2e10 | units.MSun 
+        gal_lmc.position = conv_coord(80.8958, -69.7561, 49.97)
+        gal_lmc.velocity = (47,242,225) * (1 | units.kms)
+        
+        gal_smc = gals[1]
+        gal_smc.mass = 6e9 | units.MSun 
+        gal_smc.position = conv_coord(13.1583, -72.8, 61.7)
+        gal_smc.velocity = (5.35,164,136) * (1 | units.kms)
+        return gals
+    
+    def gal_path_init(self):
+        x_lmc = [] | units.kpc
+        y_lmc = [] | units.kpc
+        z_lmc = [] | units.kpc
+        
+        x_smc = [] | units.kpc
+        y_smc = [] | units.kpc
+        z_smc = [] | units.kpc
+        return x_lmc, y_lmc, z_lmc, x_smc, y_smc, z_smc
