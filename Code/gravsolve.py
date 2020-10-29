71fee9efcb65c43e9a154262d07aa41778ca18a3
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-29 21:49:09 +0100

    Update gravsolve.py

--- Code/gravsolve.py
+++ Code/gravsolve.py
@@ -1,43 +1,55 @@
-# This code needs galpot_init, particle_init_test, evol and conv_coord to run
-from matplotlib import pyplot
-from amuse.plot import *
 from evol import neut_gal_evol
-from capture_check import capture_check
-from plot_potential import pplot
-import pandas as pd
+from plotters import plot_neut_pos, plot_mw_iso, plot_MC
+import matplotlib.pyplot as plt
 
-X_gal, X_neut, N = neut_gal_evol(old_pos='old')
+build_neut_set = input('generate a new set of neutron stars (y|n):')
+plot_data = input('plot (y|n):')
 
-plot(X_gal[0], X_gal[1], lw=0.7, label='LMC')
-plot(X_gal[3], X_gal[4], lw=0.7, label='SMC')
-plot(X_gal[6], X_gal[7], lw=0.7, label='NGC 1783')
+if build_neut_set == 'y':
+    pos = input('old or current position (old|current):')
+    neut_gal_evol(old_pos=pos)
 
-check = capture_check(X_neut)
+if plot_data =='y':
+    fix = input('fix axis (x|y|z):')
+    neut_plot = input('plot neutron stars (y|n):')
+    
+    if fix == 'z':
+        MW_plot = input('plot milky way isopotential lines ((x,y) only) (y|n):')
+    else:
+        MW_plot = 'n'
+        
+    MC_plot = input('plot Magellanic Cloud trajectories (y|n):')
+    
+    if neut_plot =='y':
+        use_all = input('plot all neutron stars (y|n):')
+        lines = plot_neut_pos()
+        lines.get_dataframe_from_pkl(dat_str='neut_stars_positions.pkl')
+        check = open('check.txt', 'r')
+        lines.plot(use_all=use_all, check = check, fix=fix)
+            
+    if MW_plot == 'y':
+        plot_mw_iso()
 
-print(check)
-
-#pyplot.gca().set_aspect("equal", adjustable="box")
-# plot the isocontour 
-from amuse.ext.galactic_potentials import MWpotentialBovy2015
-MW = MWpotentialBovy2015()
-omega = 600 | units.km/units.s/units.km
-effective_iso_potential_plot(MW, omega, center_of_rotation = [0, 0] | units.kpc,
-                                  xlim = [-10, 10] | units.kpc, ylim = [-10, 10] | units.kpc,
-                                  resolution = [1000, 1000], fraction_screen_filled = 0.8,
-                                  quadratic_contour_levels = True, contour_kwargs = dict(linestyles = 'solid',
-                                  linewidths=0.5, cmap=('Greys')), omega2 = None, center_of_rotation2 = [0, 0]|units.kpc, 
-                                  fraction_screen_filled2 = 0.2, projection3D = False, number_of_contours = 15)
-
-#pyplot.colorbar()
-
-pyplot.xlabel(r"$x$ coordinate (kpc)")
-pyplot.ylabel(r"$y$ coordinate (kpc)")
-pyplot.title("MSP's captured by Milky Way for N={} ejected MSP's".format(N))
-#pyplot.xlim(-20,20)
-#pyplot.ylim(-50,50)
-
-pyplot.legend()
-pyplot.savefig("EvolutionSystem", dpi=300)
-pyplot.show()
-
-#pplot(X_gal, X_gal, 0.05, 50, 1)
+    if MC_plot == 'y':
+        plot_MC(fix=fix)
+        
+    if use_all=='y' :
+        plt.title('Ejected MSPs from LMC')
+        
+    if use_all=='n': 
+        plt.title('Ejected MSPs close encounters')
+        
+    if fix == 'z':
+        plt.xlabel(r"$x$ coordinate (kpc)")
+        plt.ylabel(r"$y$ coordinate (kpc)")
+        
+    if fix == 'x':
+        plt.xlabel(r"$y$ coordinate (kpc)")
+        plt.ylabel(r"$z$ coordinate (kpc)")
+        
+    if fix == 'y':
+        plt.xlabel(r"$x$ coordinate (kpc)")
+        plt.ylabel(r"$z$ coordinate (kpc)")
+    plt.legend(loc='lower right')
+    plt.savefig("neutron_star_trajectories", dpi=300)
+    plt.show()
