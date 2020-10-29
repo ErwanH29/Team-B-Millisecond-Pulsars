2bc4800eb68e5bf4ccce3ae4df822cde82551ee5
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-29 18:12:00 +0100

    Update plot_potential.py

--- Code/plot_potential.py
+++ Code/plot_potential.py
@@ -18,7 +18,7 @@ def pplot(gal_MC, grid_size_in_kpc, xy_bound, i, **options):
         
         dx, dy = grid_size_in_kpc, grid_size_in_kpc
         y, x = np.mgrid[slice(-xy_bound, xy_bound + dy, dy), slice(-xy_bound, xy_bound + dx, dx)]
-        uy, ux = y | units.kpc, x | units.kpc
+        uy, ux = y | units.kpc, x | units.kpcy
         p = MWG.get_potential_at_point(0, 0 | units.kpc, ux, uy).value_in(units.m**2 * units.s**-2) + \
             LMC.get_potential_at_point(0, 0 | units.kpc, ux, uy).value_in(units.m**2 * units.s**-2) + \
             SMC.get_potential_at_point(0, 0 | units.kpc, ux, uy).value_in(units.m**2 * units.s**-2)
@@ -50,12 +50,20 @@ def pplot(gal_MC, grid_size_in_kpc, xy_bound, i, **options):
             SMC.get_potential_at_point(0, ux, uy, 0 | units.kpc).value_in(units.m**2 * units.s**-2)
         # remove the bound value
         p = p[:-1, :-1]
-
-    
-
+        
     plt.imshow(p,aspect='auto',origin='lower',extent=(x.min(),x.max(),y.min(),y.max()))
-    plt.ylabel('z in [kpc]')
-    plt.xlabel('x in [kpc]')
+    
+    if options.get('fix') == 'z':
+        plt.ylabel('y in [kpc]')
+        plt.xlabel('x in [kpc]')
+        
+    if options.get('fix') == 'x':
+        plt.ylabel('z in [kpc]')
+        plt.xlabel('y in [kpc]')
+        
+    if options.get('fix') == 'y':
+        plt.ylabel('z in [kpc]')
+        plt.xlabel('x in [kpc]')
     print('potential@{}'.format(i))
     plt.savefig('pot_plots/pot_evol_{}.png'.format(i),dpi=300)
 
