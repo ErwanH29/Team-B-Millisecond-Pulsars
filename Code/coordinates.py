b54b33376ac8394a2afffbeb235995e69ce852f6
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-29 21:47:33 +0100

    Update and rename conv_coord.py to coordinates.py

--- Code/coordinates.py
+++ Code/coordinates.py
@@ -1,13 +1,13 @@
+# IMPORTANT: use heliocentric input units (degree, degree, kiloparsec)!!
+# Returns: galactocentric x, y and z distance in kiloparsec
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import astropy.coordinates as coord
 import numpy as np
 from amuse.lab import units
 
-# IMPORTANT: use heliocentric input units (degree, degree, kiloparsec)!!
-# Returns: galactocentric x, y and z distance in kiloparsec
-
 def conv_coord(right_ascension, declination, dist):
+
     coordinates = SkyCoord(ra=right_ascension*u.degree, 
                            dec=declination*u.degree, 
                            distance=dist*u.kpc)
@@ -44,3 +44,38 @@ def prop_motion(pm_right_ascension, pm_declination, rad_velocity):
     #v_co = (vx_co, vy_co, vz_co) | units.km/units.s
     v_co = proper_motion_co
     return v_co
+
+def old_coordlist():
+    LMC_coord = (-32.2877571477, -201.334477302, -194.233794365) | units.kpc
+    LMC_vel = (-23.6377151397, -116.106444922, -126.743869139) | units.kms
+    SMC_coord = (-35.2794369809, -140.65635819, -96.9363051162) | units.kpc
+    SMC_vel = (-49.5945188999, -49.9087554726, -12.7823855815) | units.kms
+    ngc_coord = (-30.3480687709, -203.481152417, -197.606147399) | units.kpc
+    ngc_vel = (26.7697951421, -130.939929951, -234.017971574) | units.kms
+    
+    data = (LMC_coord, LMC_vel, SMC_coord, SMC_vel, ngc_coord, ngc_vel)
+    return data
+
+def capture_check(L_neut):
+    
+    check = []
+    
+    x_upp = 20 | units.kpc
+    x_low = -20 | units.kpc
+    y_upp = 20 | units.kpc
+    y_low = -20 | units.kpc
+    z_upp = 20 | units.kpc
+    z_low = -20 | units.kpc
+    
+    last_col = L_neut.iloc[:,-1]
+    for i in range(len(last_col)):
+        
+        pos_check = last_col.iloc[i][0]
+
+        if (x_low <= pos_check[0] <= x_upp and \
+            y_low <= pos_check[1] <= y_upp and \
+                z_low <= pos_check[2] <= z_upp):
+            
+            check.append(i)
+        
+    return check
