b7a212d367893045d53d8b9d1007d26272d749da
ErwanH29 <71374941+ErwanH29@users.noreply.github.com>
2020-10-31 09:58:57 +0100

    Add files via upload

--- Data/Histograms.py
+++ Data/Histograms.py
@@ -10,7 +10,7 @@ def ProbFuncReal(vel):
 
 #Velocities
 data = np.loadtxt("Data/VelocityOnlyPulsars.txt", comments="#", usecols=(1)) #Importing milisecond pulsar data
-
+print(data)
 x = np.linspace(0, max(data), len(data))
 v = np.linspace(0,1200,4000)
 dx = 1200/4000
@@ -36,32 +36,17 @@ plt.show()
 
 #Distances
 data = np.loadtxt("Data/PulsarsDataATNF(2019-04-24).txt", comments="#", usecols=(6,7,8)) #Importing milisecond pulsar data
-
-#data = np.stack((data[:,1], data[:,2], data[:,0]), axis=1)
-#galactocentr = SkyCoord(x=data[:,0], 
-#                       y=data[:,1], 
-#                       z=data[:,2], 
-#                       unit='kpc', 
-#                       representation_type='cartesian')
-#distanceMSP = galactocentr.transform_to(coord.Galactocentric)
-#x_coMSP = np.array(distanceMSP .cartesian.x)
-#y_coMSP = np.array(distanceMSP .cartesian.y)
-#z_coMSP = np.array(distanceMSP .cartesian.z)
-#MSPCoords = np.stack((z_coMSP, x_coMSP, y_coMSP), axis=1)
-
 distancemod = [(abs(np.sqrt(np.sum(data[i]**2))-8.2)) for i in range(len(data))]
 
 plt.title("Histogram of Millisecond Pulsar Distances \n From Galactic Center as Observed by the ATNF")
 n, bins, patches = plt.hist(distancemod, 120, color = 'black', histtype='step')
-plt.xlim(np.min(distancemod),20)
+plt.xlim(np.min(distancemod),35)
 plt.xlabel(r"Distance (kpc) ")
 plt.savefig("HistogramDistance", dpi = 300)
 plt.show()
 
-#distancemod = [np.sqrt(np.sum(MSPCoords[i]**2)) for i in range(len(MSPCoords))]
-#plt.title("Histogram of Pulsar Velocities as Observed by the ATNF")
-#n, bins, patches = plt.hist(distancemod, 30, color = 'black', histtype='step')
-#plt.xlim(np.min(distancemod),np.max(distancemod))
-#plt.xlabel(r"Velocity ($km s^{-1}$) ")
-#plt.savefig("HistogramDistance", dpi = 300)
-#plt.show()
+
+beyondMW = [i for i in distancemod if i>15]
+
+print("Total Millisecond Pulsars: ", len(data))
+print("Total Millisecond Pulsars beyond the Milky Way:", len(beyondMW))
