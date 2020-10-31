ab6e8ebcd2105f3e2da611dfd5bac7d459b4ac6a
arend95 <71379402+arend95@users.noreply.github.com>
2020-10-31 01:13:38 +0100

    Update gravsolve.py

--- Code/gravsolve.py
+++ Code/gravsolve.py
@@ -1,9 +1,16 @@
 from evol import neut_gal_evol
-from plotters import plot_neut_pos, plot_mw_iso, plot_MC
+from plotters import plot_neut_pos, plot_mw_iso, plot_MC, plot_hist
 import matplotlib.pyplot as plt
+from coordinates import get_final_coord
+
+print('Neutron star launcher interface. Here, neutron star sets can be generated and plotted. Also, older sets can be plotted. \n \
+If you want to plot older sets instead of generating a new one, make sure a "neut_stars_positions.pkl" \n \
+and its corresponding "check.txt" and "gal_line.pickle" are present in the current working directory. \n \
+If a new set is generated, these files will be built automatically.')
 
 build_neut_set = input('generate a new set of neutron stars (y|n):')
-plot_data = input('plot (y|n):')
+plot_data = input('plot data (y|n):')
+plot_encounters_hist = input('plot close encounters histogram (y|n):')
 
 if build_neut_set == 'y':
     pos = input('old or current position (old|current):')
@@ -56,4 +63,15 @@ if plot_data =='y':
         plt.ylabel(r"$z$ coordinate (kpc)")
         plt.savefig("neutron_star_trajectories_xz", dpi=300)
     
+    plt.show()
+
+if plot_encounters_hist == 'y':
+    
+    check = open('check.txt', 'r')
+    final_coordlist = get_final_coord(check=check)
+    plot_hist(final_coordlist)
+    plt.xlabel('Galactocentric distance in (kpc)')
+    plt.ylabel('# of MSPs')
+    plt.title('Ejected MSPs close encounters histogram')
+    plt.savefig("encounters_hist", dpi=300)
     plt.show()
