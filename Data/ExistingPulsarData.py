508f090dbebb9d5e1b06775a23215461ee5bc356
ErwanH29 <71374941+ErwanH29@users.noreply.github.com>
2020-12-06 15:00:28 +0100

    Delete ExistingPulsarData.py

--- Data/ExistingPulsarData.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.loadtxt("Desktop\PulsarsDataATNF(2019-04-24).txt", comments="#", usecols=(6,7, 8)) #Importing milisecond pulsar data
-for i in range(0,len(data)):
-    plt.scatter(data[i,1], data[i,2])
-plt.show()
